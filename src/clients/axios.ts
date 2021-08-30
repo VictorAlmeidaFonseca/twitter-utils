b1b362ec7ccc5d0daa01c386bdcb0943bd0c6c8d
VictorAlmeidaFonseca <fonseca.almeida.victor@gmail.com>
2021-08-30 18:44:40 -0300

    feat(axios): add axios as client http

---
+++ src/clients/axios.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { IHttpClient, IRequest, IResponse } from './interfaces/http-clients'
+
+export default class AxiosClient implements IHttpClient {    
+    public async request(options: IRequest): Promise<IResponse> {
+        const response = await axios.request(options) 
+        return {
+            body: response.data,
+            statusCode: response.status
+        }      
+    }  
+}
+
---
+++ test/clients/axios.test.ts
@@ -0,0 +1,15 @@
+
+import AxiosClient from '../../src/clients/axios'
+
+const axios = new AxiosClient()
+const request = {
+    baseURL : "https://www.google.com.br"
+}
+
+describe('Teste Axios as external library', () => {   
+    test('fetch external from axios', async () => {
+        const response = await axios.request(request)
+        const statusCodeValidade = String(response.statusCode)
+        expect(statusCodeValidade.startsWith('2')).toBe(true)
+    })
+})
